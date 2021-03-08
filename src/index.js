3db3d3c2102714777800bd1da47252318167019a
unknown <lerk1512@gmail.com>
2021-03-08 20:21:34 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n.toString();
+    let strMas = str.split('');
+    let result = [];
+    let j = 0;
+    for (let i = strMas.length; i >= 0; i--) {
+        result[j++] = strMas[i];
+    }
+    if (n < 0) {
+        result.pop();
+    }
+    return +result.join('');
 }
